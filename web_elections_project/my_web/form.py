779b39c9563ce0a890f81afd58e3876724dd78e1
frokes <s.andreichev@yandex.ru>
2023-01-29 14:21:12 +0300

    Поправил доступ, начал добавлять типы опросов

--- web_elections_project/my_web/form.py
+++ web_elections_project/my_web/form.py
@@ -1,7 +1,8 @@
 from django import forms
 
-
-class MyForm (forms.Form):
+CHOICES1 = [('option1', 'Один правильный ответ'), ('option2','Несколько правильных ответов')]
+CHOICES2 = []
+class MyForm(forms.Form):
     text_input = forms.CharField()
 
     answer1 = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Введите вариант ответа:'}))
@@ -9,11 +10,15 @@ class MyForm (forms.Form):
     answer3 = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Введите вариант ответа:'}))
     answer4 = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Введите вариант ответа:'}))
     answer5 = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Введите вариант ответа:'}))
-
-
+    mode = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES1)
 class MultyForm (forms.Form):
     checkbox1 = forms.BooleanField(widget=forms.CheckboxInput, required=False)
     checkbox2 = forms.BooleanField(widget=forms.CheckboxInput, required=False)
     checkbox3 = forms.BooleanField(widget=forms.CheckboxInput, required=False)
     checkbox4 = forms.BooleanField(widget=forms.CheckboxInput, required=False)
     checkbox5 = forms.BooleanField(widget=forms.CheckboxInput, required=False)
+
+class OneChoiceForm (forms.Form):
+    radiobuttons = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES2)
+
+
